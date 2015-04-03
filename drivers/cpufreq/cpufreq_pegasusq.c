d0f03b765e4a229333b2f29d25f0113108d8c70c
JustAkan <puppy1900@naver.com>
2015-04-03 15:02:56 -0700

    Defconfig,Mpdecision,cpufreq:1.5 Upgrade
    Signed-off-by: JustAkan <puppy1900@naver.com>

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -57,9 +57,9 @@
 #define DEF_START_DELAY				(0)
 
 #define UP_THRESHOLD_AT_MIN_FREQ		(40)
-#define FREQ_FOR_RESPONSIVENESS			(1512000)
+#define FREQ_FOR_RESPONSIVENESS			(1350000)
 /* for fast decrease */
-#define FREQ_FOR_FAST_DOWN			(1190400)
+#define FREQ_FOR_FAST_DOWN			(1134000)
 #define UP_THRESHOLD_AT_FAST_DOWN		(90)
 
 static unsigned int min_sampling_rate;

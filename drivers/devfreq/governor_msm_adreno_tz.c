a909aea765a4aa4d73c7a2660b5354e4380f9ab3
nazunamoe <jpg3927@gmail.com>
2015-06-28 19:07:19 +0900

    Revert "cleanup"
    This reverts commit 2d08dcbf21fdc2e7c23d7faaddcd6a917f4546ad.

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -128,6 +128,9 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 
 	*freq = stats.current_frequency;
 	*flag = 0;
+<<<<<<< HEAD
+=======
+
 	/*
 	 * Force to use & record as min freq when system has
 	 * entered pm-suspend or screen-off state.
@@ -143,6 +146,8 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		return 0;
 	}
 #endif
+
+>>>>>>> 28c79e6... Introduce Adreno idler for devfreq-based Adreno devices
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
 	if (priv->bus.num) {
